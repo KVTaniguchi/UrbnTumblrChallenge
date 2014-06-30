eca9945d37d7149677b8c0ded95ddc2c88b09197
KVTaniguchi <kv.taniguchi@gmail.com>
2014-06-30 12:16:10 -0400

    Initial Commit

---
+++ UrbnTumblrChallenge/KTViewController.h
@@ -0,0 +1,13 @@
+//
+//  KTViewController.h
+//  UrbnTumblrChallenge
+//
+//  Created by Kevin Taniguchi on 6/30/14.
+//  Copyright (c) 2014 Taniguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KTViewController : UIViewController
+
+@end
---
+++ UrbnTumblrChallenge/KTViewController.m
@@ -0,0 +1,29 @@
+//
+//  KTViewController.m
+//  UrbnTumblrChallenge
+//
+//  Created by Kevin Taniguchi on 6/30/14.
+//  Copyright (c) 2014 Taniguchi. All rights reserved.
+//
+
+#import "KTViewController.h"
+
+@interface KTViewController ()
+
+@end
+
+@implementation KTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
