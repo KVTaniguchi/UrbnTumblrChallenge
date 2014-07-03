86cfb4bad3c8f2622d0f5f4585709bea06a1b4b9
KVTaniguchi <kv.taniguchi@gmail.com>
2014-07-02 23:57:11 -0400

    adding or removing pods
    removed magical record, RZTransitions

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// RZTransitions
+#define COCOAPODS_POD_AVAILABLE_RZTransitions
+#define COCOAPODS_VERSION_MAJOR_RZTransitions 1
+#define COCOAPODS_VERSION_MINOR_RZTransitions 0
+#define COCOAPODS_VERSION_PATCH_RZTransitions 0
+
